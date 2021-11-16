63750d2fad7a37504eb7684709e54f0b4c7edb70
Oskar Słyk <oskar.slyk@mavoco.com>
2021-11-16 21:48:51 +0100

    First repository test

--- src/test/java/com/oskarro/comparator/ComparatorApplicationTests.java
+++ src/test/java/com/oskarro/comparator/ComparatorApplicationTests.java
@@ -3,12 +3,18 @@ package com.oskarro.comparator;
 import com.oskarro.comparator.model.Product;
 import com.oskarro.comparator.model.ProductType;
 import com.oskarro.comparator.repository.ProductRepository;
+import org.assertj.core.groups.Tuple;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Spliterator;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,22 +25,30 @@ class ComparatorApplicationTests {
     ProductRepository productRepository;
 
     @Test
-    public void shouldSaveProduct() {
+    void shouldSaveProduct() {
+        // GIVEN
         Product product = new Product(UUID.randomUUID().toString(), "PRODUCT CREATE", ProductType.FOOD);
+
+        // WHEN
         Product saved = productRepository.save(product);
+
+        // THEN
         assertThat(saved).isNotNull();
         assertThat(productRepository.count()).isGreaterThan(0);
     }
 
     @Test
-    public void shouldGetSavedProduct() {
+    void shouldGetSavedProduct() {
+        // GIVEN
         String id = UUID.randomUUID().toString();
         Product product = new Product(id, "PRODUCT GET", ProductType.FOOD);
 
+        // WHEN
         productRepository.save(product);
         Optional<Product> productOptional = productRepository.findById(id);
-        assertThat(productOptional).isPresent();
 
+        // THEN
+        assertThat(productOptional).isPresent();
         Product saved = productOptional.get();
         assertThat(saved).isNotNull();
         assertThat(saved.getProductType()).isEqualTo(ProductType.FOOD);
@@ -42,22 +56,58 @@ class ComparatorApplicationTests {
     }
 
     @Test
-    public void shouldUpdateSavedProduct() {
+    void shouldUpdateSavedProduct() {
+        // GIVEN
         String id = UUID.randomUUID().toString();
         Product product = new Product(id, "PRODUCT UPDATE", ProductType.FOOD);
 
+        // WHEN
         Product saved = productRepository.save(product);
         saved.setProductType(ProductType.ALCOHOL);
         productRepository.save(product);
 
+        // THEN
         Optional<Product> productOptional = productRepository.findById(id);
         assertThat(productOptional).isPresent();
-
         Product updated = productOptional.get();
         assertThat(updated).isNotNull();
         assertThat(updated.getName()).isEqualTo("PRODUCT UPDATE");
         assertThat(updated.getProductType()).isEqualTo(ProductType.ALCOHOL);
     }
+
+    @Test
+    void shouldDeleteSavedProduct() {
+        // GIVEN
+        String id = UUID.randomUUID().toString();
+        Product product = new Product(id, "PRODUCT DELETE", ProductType.FOOD);
+
+        // WHEN
+        productRepository.save(product);
+        productRepository.deleteById(id);
+
+        // THEN
+        assertThat(productRepository.findById(id)).isEmpty();
+    }
+
+    @Test
+    void shouldFindAllProduct() {
+        // GIVEN
+        Product productOne = new Product(UUID.randomUUID().toString(), "PRODUCT FIND ALL 1", ProductType.FOOD);
+        Product productTwo = new Product(UUID.randomUUID().toString(), "PRODUCT FIND ALL 2", ProductType.OTHER);
+
+        // WHEN
+        productRepository.save(productOne);
+        productRepository.save(productTwo);
+        Spliterator<Product> productSpliterator = productRepository.findAll().spliterator();
+        List<Product> products = StreamSupport.stream(productSpliterator, false).collect(Collectors.toList());
+
+        // THEN
+        assertThat(products.size()).isGreaterThan(1);
+        assertThat(products).extracting("name", "productType").contains(Tuple.tuple("PRODUCT FIND ALL 1", ProductType.FOOD));
+        assertThat(products).extracting("name", "productType").contains(Tuple.tuple("PRODUCT FIND ALL 2", ProductType.OTHER));
+    }
+
+
 
 
 
