ad2c4084eae9e5982614affb6fa59b35cfbc5f47
Oskar Słyk <oskar.slyk@mavoco.com>
2021-12-07 18:23:10 +0100

    method for get string

--- src/main/java/com/oskarro/comparator/gateway/RedisGateway.java
+++ src/main/java/com/oskarro/comparator/gateway/RedisGateway.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.AbstractMap;
 import java.util.Map;
 
 @RestController
@@ -21,5 +23,14 @@ public class RedisGateway {
     public Map.Entry<String, String> setString(@RequestBody Map.Entry<String, String> kvp) {
         template.opsForValue().set(STRING_KEY_PREFIX + kvp.getKey(), kvp.getValue());
         return kvp;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/string/{key}")
+    public Map.Entry<String, String> getString(@PathVariable("key") String key) {
+        String value = template.opsForValue().get(STRING_KEY_PREFIX + key);
+        if (value == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "key not found");
+        }
+        return new AbstractMap.SimpleEntry<>(key, value);
     }
 }
